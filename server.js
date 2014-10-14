e05a43145595d3961868eb90fdb2d40be437f8be
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-10-13 22:14:53 -0400

    Working Social Sign-in

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+
+app.use(session({secret:'sessionsecret'})); //export into private file
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport);
+
+// 404 ERROR RESPONSE
+app.use(function(req, res) {
+   res.send('404: Page not Found', 404);
+});
+
+// 500 ERROR RESPONSE
+app.use(function(error, req, res, next) {
+   res.send('500: Internal Server Error', 500);
+});
+
+app.listen(port);
+console.log('Server running on ' + port);
